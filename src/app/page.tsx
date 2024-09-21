7fbc575d81c9ed3a982ef96f2b6cd3f36aa6d4d7
sha-shak <shahriar.shakhawat@gmail.com>
2024-09-21 11:48:40 +0600

    fix: prettier and husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 
 export default function Home() {
-  console.log("Hello");
+  console.log("Hello world");
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
+          alt="NextJS logo"
           width={180}
           height={38}
           priority
@@ -21,7 +21,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Saves and see your changes instantly.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
