0094c9df8af599468e9b4a4c189315d1efec3b55
noelalam9999 <noel.alam9999@gmail.com>
2025-02-16 20:52:12 +0600

    feat:see more home page redirection

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
-export const config = {
-  backendURL: 'http://localhost:8080',
-};
 // export const config = {
-//   backendURL: 'https://infinimed-launch-1.fly.dev',
+//   backendURL: 'http://localhost:8080',
 // };
+export const config = {
+  backendURL: 'https://infinimed-launch-1.fly.dev',
+};
 // https://infinimed-launch-1.fly.dev/
